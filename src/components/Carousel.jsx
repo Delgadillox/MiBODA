a89a0ac26bf0dc0cafc146f53aa585b4ef1c3ce4
Delgadillox <josepablodelgadillo@gmail.com>
2024-02-16 10:53:33 -0600

    Fix

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,80 +1,84 @@
-// import * as React from 'react';
-// import { useTheme } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import MobileStepper from '@mui/material/MobileStepper';
-// import SwipeableViews from 'react-swipeable-views';
-// import { autoPlay } from 'react-swipeable-views-utils';
-// import salon1 from "../assets/salon1.jpg"
-// import salon2 from "../assets/salon2.jpg"
-// import salon3 from "../assets/salon3.jpg"
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import SwipeableViews from 'react-swipeable-views-react-18-fix';
+import salon1 from "../assets/salon1.jpg"
+import salon2 from "../assets/salon2.jpg"
+import salon3 from "../assets/salon3.jpg"
 
 
-// const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
-// const images = [
-//   {
-//     label: 'Salon1',
-//     imgPath: salon1,
-//   },
-//   {
-//     label: 'Salon2',
-//     imgPath: salon2,
-//   },
-//   {
-//     label: 'Salon3',
-//     imgPath: salon3,
-//   }
-// ];
+const images = [
+  {
+    label: 'Salon1',
+    imgPath: salon1,
+  },
+  {
+    label: 'Salon2',
+    imgPath: salon2,
+  },
+  {
+    label: 'Salon3',
+    imgPath: salon3,
+  }
+];
 
-// function Carousel() {
-//   const theme = useTheme();
-//   const [activeStep, setActiveStep] = React.useState(0);
-//   const maxSteps = images.length;
+function Carousel() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
 
-//   const handleStepChange = (step) => {
-//     setActiveStep(step);
-//   };
+  setTimeout(() => {
+    setActiveStep(
+      activeStep === images.length - 1 ? 0 : activeStep + 1
+    );
+  }, 5000);
 
-//   return (
-//     <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
 
-//       <AutoPlaySwipeableViews
-//         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-//         index={activeStep}
-//         onChangeIndex={handleStepChange}
-//         enableMouseEvents
-//       >
-//         {images.map((step, index) => (
-//           <div key={step.label}>
-//             {Math.abs(activeStep - index) <= 2 ? (
-//               <Box
-//                 component="img"
-//                 sx={{
-//                   height: 255,
-//                   display: 'block',
-//                   maxWidth: 400,
-//                   overflow: 'hidden',
-//                   width: '100%',
-//                 }}
-//                 src={step.imgPath}
-//                 alt={step.label}
-//               />
-//             ) : null}
-//           </div>
-//         ))}
-//       </AutoPlaySwipeableViews>
-//       <MobileStepper
-//         steps={maxSteps}
-//         position="static"
-//         style={{
-//             marginLeft:"40%",
-//             marginRight:"50%",
-//             backgroundColor: "rgba(255,0,0,0)"
-//         }}
-//         activeStep={activeStep}
-//       />
-//     </Box>
-//   );
-// }
+  return (
+    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
 
-// export default Carousel;
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 255,
+                  display: 'block',
+                  maxWidth: 400,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </SwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        style={{
+            marginLeft:"40%",
+            marginRight:"50%",
+            backgroundColor: "rgba(255,0,0,0)"
+        }}
+        activeStep={activeStep}
+      />
+    </Box>
+  );
+}
+
+export default Carousel;
