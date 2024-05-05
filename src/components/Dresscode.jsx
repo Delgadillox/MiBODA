761733b6efae60f4b431685dd0f372f1f5402f08
Delgadillox <josepablodelgadillo@gmail.com>
2024-05-04 23:20:30 -0600

    colores

--- src/components/Dresscode.jsx
+++ src/components/Dresscode.jsx
@@ -1,44 +1,81 @@
-import '../styles/dresscode.css'
-import riuImage from "../assets/riu.png";
-import pbImage from "../assets/pueblobonito.png";
-import cerritosImage from "../assets/descarga.jpg";
-import codeImage from "../assets/vestimenta.png";
+import '../styles/dresscode.css';
+import riuImage from '../assets/riu.png';
+import pbImage from '../assets/pueblobonito.png';
+import cerritosImage from '../assets/descarga.jpg';
+import codeImage from '../assets/vestimenta.png';
 
 const Dresscode = () => {
-  const colores = ['#E1CEB9', '#C8B39C', '#CBDDFF', '#9FBFE4']
-  return (
-    <div name="dresscode" className="dresscode">
-        <div className="content">
-          <div className="top-content">
-            <span className="title">Código de Vestimenta</span><br/><br/>
-            <br />
-            <span className='text'>FORMAL PLAYA</span><br/>
-            <img className="codeImage" src={codeImage} alt='desscode'/><br/>
-            <span className="text">PALETA DE COLORES</span><br/>
-            <div className="container">
-              {colores.map((color, index) => (
-                <div key={index} className="color" style={{ backgroundColor: color }}></div>
-              ))}
-            </div>
+	const colores = [
+		'rgb(201 173 141)',
+		'rgb(145 127 107)',
+		'rgb(147 166 201)',
+		'rgb(94 120 150)',
+	];
+	return (
+		<div name='dresscode' className='dresscode'>
+			<div className='content'>
+				<div className='top-content'>
+					<span className='title'>Código de Vestimenta</span>
+					<br />
+					<br />
+					<br />
+					<span className='text'>FORMAL PLAYA</span>
+					<br />
+					<img className='codeImage' src={codeImage} alt='desscode' />
+					<br />
+					<span className='text'>PALETA DE COLORES</span>
+					<br />
+					<div className='container'>
+						{colores.map((color, index) => (
+							<div
+								key={index}
+								className='color'
+								style={{ backgroundColor: color }}
+							></div>
+						))}
+					</div>
 
-            {/* <img src={paletaColores} className='paletaImg'/><br/> */}
-            
-            <br/>
-            <span className='redText'>NO USAR COLOR BLANCO</span>
-          </div>
-          <br/>
+					{/* <img src={paletaColores} className='paletaImg'/><br/> */}
 
-          <div className="bottom-content" >
-            <span className='title'>Hospedaje Sugerido</span><br/><br /><br />
-            <a href='https://www.riu.com/es/hotel/mexico/mazatlan/hotel-riu-emerald-bay/' target='_blank' style={{ padding: '10px' }}><img src={riuImage} alt="riu" height="94" width="100"/></a>
-            <a href='https://www.cerritosresort.com/' target='_blank' style={{ padding: '10px' }}><img src={cerritosImage} alt="cer" width="100"/></a>
-            <a href='https://www.pueblobonito.com.mx/resorts/emerald-bay' target='_blank' style={{ padding: '10px' }}><img src={pbImage} alt="pb" width="150"/></a>
-            <br/>
-            <span className='text'>(Da click en cualquier logotipo para ir al sitio web del hotel)</span>
-          </div>
-        </div>
-    </div>
-  )
-}
+					<br />
+					<span className='redText'>NO USAR COLOR BLANCO</span>
+				</div>
+				<br />
 
-export default Dresscode
+				<div className='bottom-content'>
+					<span className='title'>Hospedaje Sugerido</span>
+					<br />
+					<br />
+					<br />
+					<a
+						href='https://www.riu.com/es/hotel/mexico/mazatlan/hotel-riu-emerald-bay/'
+						target='_blank'
+						style={{ padding: '10px' }}
+					>
+						<img src={riuImage} alt='riu' height='94' width='100' />
+					</a>
+					<a
+						href='https://www.cerritosresort.com/'
+						target='_blank'
+						style={{ padding: '10px' }}
+					>
+						<img src={cerritosImage} alt='cer' width='100' />
+					</a>
+					<a
+						href='https://www.pueblobonito.com.mx/resorts/emerald-bay'
+						target='_blank'
+						style={{ padding: '10px' }}
+					>
+						<img src={pbImage} alt='pb' width='150' />
+					</a>
+					<br />
+					<span className='text'>
+						(Da click en cualquier logotipo para ir al sitio web del hotel)
+					</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Dresscode;
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,103 +1,116 @@
-import { useEffect, useState } from "react"
-import '../styles/main.css'
+import { useEffect, useState } from 'react';
+import '../styles/main.css';
 
 const Start = () => {
-    const [time, setTime] = useState({
-        days: 0,
-        hours: 0,
-        minutes: 0,
-        seconds: 0
-    });
-      
-    const calculateTimeLeft = () => {
-        const targetDate = new Date('2024-11-30T17:00:00Z'); // Cambia esta fecha a la que desees
-        const now = new Date();
-        const difference = targetDate - now;
-      
-        if (difference > 0) {
-          const days = Math.floor(difference / (1000 * 60 * 60 * 24)).toString().padStart(2, 0);
-          const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, 0);
-          const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, 0);
-          const seconds = Math.floor((difference % (1000 * 60)) / 1000).toString().padStart(2, 0);
-      
-          setTime({
-            days,
-            hours,
-            minutes,
-            seconds
-          });
-        } else {
-          setTime({
-            days: 0,
-            hours: 0,
-            minutes: 0,
-            seconds: 0
-          });
-        }
-    };
-      
-      
-    useEffect(() => {
-        const timer = setInterval(() => {
-          calculateTimeLeft();
-        }, 1000);
-      
-        return () => clearInterval(timer);
-    }, [])
+	const [time, setTime] = useState({
+		days: 0,
+		hours: 0,
+		minutes: 0,
+		seconds: 0,
+	});
 
+	const calculateTimeLeft = () => {
+		const targetDate = new Date('2024-11-30T17:00:00Z');
+		const now = new Date();
+		const difference = targetDate - now;
 
-  return (
-    <>
-        <div className="content">
-          <div className="top-content">
-            <span className="subtitle">NUESTRA BODA</span><br/><br/>
-            <span className="title">Pablo & Mayte</span>
-          </div><br />
-          <div className="bottom-content" >
-            <span className="date">NOVIEMBRE</span> <br/>
-            <div className="fullDate">
-              <div className="dateContainer">
-                <span className="day">SÁBADO</span>
-                <span className="separator">|</span>
-                <span className="dayNumber">30</span>
-                <span className="separator">|</span>
-                <span className="hour">5 PM</span>
-              </div>
-              <span className="year">2024</span>
-            </div><br/><br/>
-            <hr/>
-            <div className="countdown">
-              <div className="days">
-                <span>{time.days}</span>
-                <p>DÍAS</p>
-              </div>
-              <div>
-                <span className="puntos">:</span>
-              </div>
-              <div className="hours">
-                <span>{time.hours}</span>
-                <p>HORAS</p>
-              </div>
-              <div>
-                <span className="puntos">:</span>
-              </div>
-              <div className="minutes">
-                <span>{time.minutes}</span>
-                <p>MINUTOS</p>
-              </div>
-              <div>
-                <span className="puntos">:</span>
-              </div>
-              <div className="seconds">
-                <span>{time.seconds}</span>
-                <p>SEGUNDOS</p>
-              </div>
-            </div>
-            <hr/>
-          </div>
-        </div>
-      </>
-  )
-}
+		if (difference > 0) {
+			const days = Math.floor(difference / (1000 * 60 * 60 * 24))
+				.toString()
+				.padStart(2, 0);
+			const hours = Math.floor(
+				(difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+			)
+				.toString()
+				.padStart(2, 0);
+			const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+				.toString()
+				.padStart(2, 0);
+			const seconds = Math.floor((difference % (1000 * 60)) / 1000)
+				.toString()
+				.padStart(2, 0);
 
-export default Start
+			setTime({
+				days,
+				hours,
+				minutes,
+				seconds,
+			});
+		} else {
+			setTime({
+				days: 0,
+				hours: 0,
+				minutes: 0,
+				seconds: 0,
+			});
+		}
+	};
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			calculateTimeLeft();
+		}, 1000);
+
+		return () => clearInterval(timer);
+	}, []);
+
+	return (
+		<>
+			<div className='content'>
+				<div className='top-content'>
+					<span className='subtitle'>NUESTRA BODA</span>
+					<br />
+					<br />
+					<span className='title'>Pablo & Mayte</span>
+				</div>
+				<br />
+				<div className='bottom-content'>
+					<span className='date'>NOVIEMBRE</span> <br />
+					<div className='fullDate'>
+						<div className='dateContainer'>
+							<span className='day'>SÁBADO</span>
+							<span className='separator'>|</span>
+							<span className='dayNumber'>30</span>
+							<span className='separator'>|</span>
+							<span className='hour'>5 PM</span>
+						</div>
+						<span className='year'>2024</span>
+					</div>
+					<br />
+					<br />
+					<hr />
+					<div className='countdown'>
+						<div className='days'>
+							<span>{time.days}</span>
+							<p>DÍAS</p>
+						</div>
+						<div>
+							<span className='puntos'>:</span>
+						</div>
+						<div className='hours'>
+							<span>{time.hours}</span>
+							<p>HORAS</p>
+						</div>
+						<div>
+							<span className='puntos'>:</span>
+						</div>
+						<div className='minutes'>
+							<span>{time.minutes}</span>
+							<p>MINUTOS</p>
+						</div>
+						<div>
+							<span className='puntos'>:</span>
+						</div>
+						<div className='seconds'>
+							<span>{time.seconds}</span>
+							<p>SEGUNDOS</p>
+						</div>
+					</div>
+					<hr />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Start;
